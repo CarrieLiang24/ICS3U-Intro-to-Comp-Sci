1c40dae6c50c4ed4b200df994087f4252a41002c
CarrieLiang24 <cliang@bayviewglen.ca>
2021-01-27 16:13:07 -0500

    Homework1

--- Unit One - Introduction to Java/src/week1/Homework1Question3.java
+++ Unit One - Introduction to Java/src/week1/Homework1Question3.java
@@ -2,6 +2,6 @@ package week1;
 
 public class Homework1Question3 {
     public static void main(String[] args) {
-        System.out.println("Bayview Glen Independent School\n85 Moatfield Dr\nNorth York,\nON M3B 3L6");
+        System.out.println("Bayview Glen Independent School\n85 Moatfield Dr\nNorth York, ON\nM3B 3L6");
         }
 }
