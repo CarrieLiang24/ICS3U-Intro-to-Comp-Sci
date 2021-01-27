568c7fece711eba060e1c1d6a3fe20e755114aaa
CarrieLiang24 <cliang@bayviewglen.ca>
2021-01-27 16:09:12 -0500

    Homework1

--- Unit One - Introduction to Java/src/week1/Homework1Question4.java
+++ Unit One - Introduction to Java/src/week1/Homework1Question4.java
@@ -7,7 +7,6 @@ public class Homework1Question4 {
         System.out.println("/\t\tOur Lady of Mercy Hospital\t\t/");
         System.out.println("/\t\t--------------------------\t\t/");
         System.out.println("/\t\t\t\t\t\t\t/");
-        
         System.out.println("/\t\t     1306 Central City\t\t\t/");
         System.out.println("/\t\t   Phone (416) 555-1212\t\t\t/");
         System.out.println("/\t\t\t\t\t\t\t/");
