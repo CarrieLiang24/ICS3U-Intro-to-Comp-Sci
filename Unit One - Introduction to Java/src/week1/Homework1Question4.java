019aeb25853c6b261da81bbf0b8e37d040c869a6
CarrieLiang24 <cliang@bayviewglen.ca>
2021-01-27 16:07:41 -0500

    Homework1

--- Unit One - Introduction to Java/src/week1/Homework1Question4.java
+++ Unit One - Introduction to Java/src/week1/Homework1Question4.java
@@ -2,15 +2,15 @@ package week1;
 
 public class Homework1Question4 {
     public static void main(String[] args) {
-        System.out.println("/////////////////////////////////////////////////");
-        System.out.println("/\t\t\t\t\t\t\t\t\t\t\t\t/");
-        System.out.println("/\t\t\tOur Lady of Mercy Hospital\t\t\t/");
-        System.out.println("/\t\t\t--------------------------\t\t\t/");
-        System.out.println("/\t\t\t\t\t\t\t\t\t\t\t\t/");
-        System.out.println("/\t\t\t\t\t\t\t\t\t\t\t\t/");
-        System.out.println("/\t\t\t\t1306 Central City\t\t\t\t/");
-        System.out.println("/\t\t\t\tPhone (416) 555-1212\t\t\t/");
-        System.out.println("/\t\t\t\t\t\t\t\t\t\t\t\t/");
-        System.out.println("/////////////////////////////////////////////////");
+        System.out.println("/////////////////////////////////////////////////////////");
+        System.out.println("/\t\t\t\t\t\t\t/");
+        System.out.println("/\t\tOur Lady of Mercy Hospital\t\t/");
+        System.out.println("/\t\t--------------------------\t\t/");
+        System.out.println("/\t\t\t\t\t\t\t/");
+        
+        System.out.println("/\t\t     1306 Central City\t\t\t/");
+        System.out.println("/\t\t   Phone (416) 555-1212\t\t\t/");
+        System.out.println("/\t\t\t\t\t\t\t/");
+        System.out.println("/////////////////////////////////////////////////////////");
         }
 }
